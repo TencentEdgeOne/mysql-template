fa532b6dad0380b6e65131db4e61677ac60508db
shirlyyang <shirlyyang@tencent.com>
2025-09-03 15:38:31 +0800

    doc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,7 +85,7 @@ export default function Home() {
               Deploy Now
             </Button>
             </a>
-            <a href="https://edgeone.ai/document/187318055541846016" target="_blank" rel="noopener noreferrer">
+            <a href="https://pages.edgeone.ai/document/node-functions" target="_blank" rel="noopener noreferrer">
             <Button 
               variant="outline" 
               size="lg" 
